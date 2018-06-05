ccaa57a7d4ccb8d4558338b96cf4db01c02bd5b4
AhmBen <ahmed.benrouag.pf1@gmail.com>
2018-06-05 09:43:29 +0200

    MAJ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Mon App';
+  welcomeMsg = 'Bonjour à tous';
+  user = {
+    id: 0,
+    name: 'Michel François',
+    image: 'https://s3.amazonaws.com/uifaces/faces/twitter/malykhinv/128.jpg',
+    jobTitle: 'Customer Division Assistant'
+  };
+  test(e: MouseEvent) {
+    console.log('user : ' + JSON.stringify(this.user));
+    console.log('user : ' + this.user.name);
+    console.log(e);
+  }
+  changeText(text) {
+    // console.log('[' + e.target.value + ']');
+    // console.log('[' + text + ']');
+    this.user.name = text;
+  }
+
+  logInputValue(v) {
+    console.log('La valeur saisie est : ' + v);
+  }
 }
